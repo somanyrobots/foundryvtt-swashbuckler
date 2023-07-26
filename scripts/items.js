a9ecde1f9dbfc5d0770f11b677b9622cd6f36be1
somanyrobots <somanyrobots@gmail.com>
2023-07-25 21:11:56 -0700

    Initial Swashbuckler Module

---
+++ scripts/items.js
@@ -0,0 +1,7 @@
+// Hooks.once('init', async function() {
+// });
+
+Hooks.once('ready', async function() {
+	CONFIG.DND5E.weaponProperties['matchlock'] = 'Matchlock';
+	CONFIG.DND5E.weaponProperties['flintlock'] = 'Flintlock';
+});
